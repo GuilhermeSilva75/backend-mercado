4f244dd9d89d0397f07bb1c383123cbd47cd3685
Guilherme Silva <guilhermedev874@gmail.com>
2024-09-10 10:47:26 -0300

    Login Adicionado

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload {
+    sub: string
+}
+
+export function isAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authToken = request.headers.authorization
+
+    if (!authToken) {
+        return response.status(401).end()
+    }
+
+    const [, token] = authToken.split(" ");
+
+  try{
+    const { sub } = verify(
+      token,
+      process.env.JWT_SECRET
+    ) as Payload;
+
+    request.user_id = sub;
+
+    return next();
+
+  }catch(err){
+    return response.status(401).end();
+  }
+}
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,4 +1,6 @@
 import { prismaClient } from "../../prisma";
+import { compare } from "bcrypt";
+import { sign } from 'jsonwebtoken'
 
 interface AuthRequest {
     email: string
@@ -8,7 +10,39 @@ interface AuthRequest {
 
 class AuthUserService {
     async execute({ email, password }: AuthRequest) {
-        
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+            throw new Error('Email/password incorret')
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error('Password incorret')
+        }
+
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            token: token
+        }
     }
 }
 
