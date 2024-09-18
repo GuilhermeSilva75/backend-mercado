26fedec018f4b6a9d8ad50a6d7f48fd313932204
Guilherme Silva <guilhermedev874@gmail.com>
2024-09-18 09:55:52 -0300

    Adicionado CORS

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction, json } from "express";
 import { router } from "./routes";
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(router)
 
